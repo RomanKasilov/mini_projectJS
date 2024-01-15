e40b0aefc775e3149e4ff60a21f6b4110e575612
РС <KasiaKasilov@gmail.com>
2024-01-15 17:36:09 +0200

    save

--- post-details/post-details.js
+++ post-details/post-details.js
@@ -11,28 +11,26 @@ for (let key in post) {
     let post_div = document.createElement('div');
     post_div.classList.add('post_div')
     let pst = document.createElement('p');
-    pst.innerText = `${key}: ${post[key]}`;
+    pst.innerHTML = `<span>${key}:</span> ${post[key]}`;
     post_wrapper.appendChild(post_div);
     post_div.appendChild(pst);
-fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
-        .then(res => res.json())
-        .then(comments =>{
-            console.log(comments);
-            let comment_wrapper = document.createElement('div');
-            comment_wrapper.classList.add('comments_wrapper');
-            wrapper.appendChild(comment_wrapper);
-            for (let comment of comments) {
-                let comment_div = document.createElement('div');
-                comment_div.classList.add('comment_div')
-                let comment_name = document.createElement('p');
-                let comment_body = document.createElement('p');
-                comment_name.innerText = `${comment.name}:`
-                comment_body.innerText = `${comment.body}`
-                comment_div.append(comment_name, comment_body);
-                comment_wrapper.appendChild(comment_div);
-            }
-
-        })
-
-
 }
+fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
+    .then(res => res.json())
+    .then(comments =>{
+        console.log(comments);
+        let comment_wrapper = document.createElement('div');
+        comment_wrapper.classList.add('comments_wrapper');
+        wrapper.appendChild(comment_wrapper);
+        for (let comment of comments) {
+            let comment_div = document.createElement('div');
+            comment_div.classList.add('comment_div')
+            let comment_name = document.createElement('p');
+            let comment_body = document.createElement('p');
+            comment_name.innerText = `${comment.name}:`
+            comment_body.innerText = `${comment.body}`
+            comment_div.append(comment_name, comment_body);
+            comment_wrapper.appendChild(comment_div);
+        }
+
+    })
